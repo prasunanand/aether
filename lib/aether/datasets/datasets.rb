14baafb4dd8a2554ed4040b5a649ca8c37c99acd
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-03-05 06:10:50 +0530

    load_digits and load_iris init

--- lib/aether/datasets/datasets.rb
+++ lib/aether/datasets/datasets.rb
@@ -1,7 +1,13 @@
 module Aether
   class Datasets
+
     def self.load_iris
       return 1
     end
+
+    def self.load_digits
+      return 1
+    end
+
   end
 end
--- spec/datasets/datasets.rb
+++ spec/datasets/datasets.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe Aether::Datasets do
-  context '#initialize' do
+  context '#load_iris' do
     let(:i) { Aether::Datasets.load_iris }
     subject { i }
     it { expect(i).to eq 1 }
   end
+
+  context '#load_digits' do
+    let(:i) {Aether::Datasets.load_digits}
+    subject { i }
+    it { expect(i).to eq 1 }
+  end
+
 end
